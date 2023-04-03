fc780585904a3a543f5a37a60ba43ea26a6f4092
Andreea Asaftei <Andreea.Sava@centric.eu>
2023-04-03 19:17:07 +0300

    added login test

--- ACAutomation/ACAutomation/LoginTests.cs
+++ ACAutomation/ACAutomation/LoginTests.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace ACAutomation
@@ -11,22 +12,35 @@ namespace ACAutomation
         public void Login_ValidCredentials_DisplaysAccountName()
         {
             //open browser
-
             var driver = new ChromeDriver();
 
             //maximize browser window
-
             driver.Manage().Window.Maximize();
 
             //navigate to application URL
-
             driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com/");
 
             //click sign in button from header
+            driver.FindElement(By.XPath("//div[@class='panel header']//a[contains(text(), 'Sign In')]")).Click();
+
             //fill in user email
+            driver.FindElement(By.Id("email")).SendKeys("test@email.ro");
+
             //fill in user password
+            driver.FindElement(By.Name("login[password]")).SendKeys("Test!123");
+
             //click sign in button
+            driver.FindElement(By.CssSelector("button[name='send']")).Click();
+
+            // sleep
+            Thread.Sleep(2000);
+
             //assert
+            Assert.AreEqual("Welcome, Test Firstname Test Lastname!",
+                driver.FindElement(By.XPath("//div[@class='panel header']//li[@class='greet welcome']/span[@class='logged-in']")).Text);
+
+            // clean-up
+            driver.Quit();
         }
     }
 }
