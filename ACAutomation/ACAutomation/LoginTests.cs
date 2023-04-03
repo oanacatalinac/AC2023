06b9c24a925b4fd892d44c17c6e42d4716398391
Cioaba, Oana <oana.cioaba@centric.eu>
2023-04-03 18:03:12 +0300

    Added Project structure

---
+++ ACAutomation/ACAutomation/LoginTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium.Chrome;
+
+namespace ACAutomation
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void Login_ValidCredentials_DisplaysAccountName()
+        {
+            //open browser
+
+            var driver = new ChromeDriver();
+
+            //maximize browser window
+
+            driver.Manage().Window.Maximize();
+
+            //navigate to application URL
+
+            driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com/");
+
+            //click sign in button from header
+            //fill in user email
+            //fill in user password
+            //click sign in button
+            //assert
+        }
+    }
+}
---
+++ ACAutomation/ACAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Centric")]
+[assembly: AssemblyProduct("ACAutomation")]
+[assembly: AssemblyCopyright("Copyright © Centric 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("514d14e3-dadc-465c-b1da-bb3c5a8602e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
