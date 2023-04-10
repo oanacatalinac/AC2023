b9c3bdeba982eeaee6b98879c3b5fc7fb8864ea3
Cioaba, Oana <oana.cioaba@centric.eu>
2023-04-10 18:28:29 +0300

    Added TestInitialize and TestCleanup. Added Login with wrong user email

--- ACAutomation/ACAutomation/LoginTests.cs
+++ ACAutomation/ACAutomation/LoginTests.cs
@@ -1,29 +1,30 @@
-﻿using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace ACAutomation
 {
     [TestClass]
     public class LoginTests
     {
-        [TestMethod]
-        public void Login_ValidCredentials_DisplaysAccountName()
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
-            //open browser
-            var driver = new ChromeDriver();
-
-            //maximize browser window
+            driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-
-            //navigate to application URL
             driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com/");
+        }
 
+        [TestMethod]
+        public void Should_LoginUser_When_ValidCredentialsAreUsed()
+        {
             //click sign in button from header
             driver.FindElement(By.XPath("//div[@class='panel header']//a[contains(text(), 'Sign In')]")).Click();
 
-            //fill in user email
+            //fill in valid user email
             driver.FindElement(By.Id("email")).SendKeys("test@email.ro");
 
             //fill in user password
@@ -38,8 +39,34 @@ namespace ACAutomation
             //assert
             Assert.AreEqual("Welcome, Test Firstname Test Lastname!",
                 driver.FindElement(By.XPath("//div[@class='panel header']//li[@class='greet welcome']/span[@class='logged-in']")).Text);
+        }
 
-            // clean-up
+        [TestMethod]
+        public void Should_NotLoginUser_When_WrongEmailIsUsed()
+        {
+            //click sign in button from header
+            driver.FindElement(By.XPath("//div[@class='panel header']//a[contains(text(), 'Sign In')]")).Click();
+
+            //fill in wrong user email
+            driver.FindElement(By.Id("email")).SendKeys("tester@outlook.com");
+
+            //fill in user password
+            driver.FindElement(By.Name("login[password]")).SendKeys("Test!123");
+
+            //click sign in button
+            driver.FindElement(By.CssSelector("button[name='send']")).Click();
+
+            // sleep
+            Thread.Sleep(2000);
+
+            //assert
+            Assert.AreEqual("The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.",
+                driver.FindElement(By.XPath("//div[@role = 'alert']/div/div")).Text);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
             driver.Quit();
         }
     }
